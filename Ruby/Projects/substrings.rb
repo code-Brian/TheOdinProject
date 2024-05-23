3f50f128ad0e84c1d13fd76556940d6ddbac0ef3
codeBrian <89549557+code-Brian@users.noreply.github.com.>
2024-05-22 23:44:16 -0700

    solved for finding matches and substring matches

--- Ruby/Projects/substrings.rb
+++ Ruby/Projects/substrings.rb
@@ -1,6 +1,26 @@
-def find_repeated_strings(string, string_array)
-  # some logic here af
+def substrings(string, dictionary)
+  # iterate through the dictionary and check if there are any matching keys
+  words = string.downcase.split(" ")
+  word_counts = Hash.new(0)
+
+  words.each do |word|
+    dictionary.each do |entry|
+      if word.include?(entry)
+        puts "match with #{word} found at dictionary entry: #{entry}."
+      end
+    end
+  end
+
+  puts word_counts
 end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", 
+  "howdy", "it", "i", "low", "own", "part", "partner", "sit" ]
+
+substrings("below I go", dictionary)
+=begin
+
+to convert a string to a symbol, use .symbol_keys
 
 string_hash = {
   :bird => 3,
@@ -14,3 +34,10 @@ string_hash = {
 string_hash.each do |key, value|
   puts "Key: #{key}, Value: #{value}"
 end
+
+puts dictionary
+=end
+
+
+
+
