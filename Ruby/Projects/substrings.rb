2dd15335e46097b7add52efc4106217b89e13b3c
Brian Denmark <89549557+code-Brian@users.noreply.github.com>
2024-05-22 18:23:57 -0700

    added some rough draft ideas, will finish soon.

--- Ruby/Projects/substrings.rb
+++ Ruby/Projects/substrings.rb
@@ -1,3 +1,16 @@
 def find_repeated_strings(string, string_array)
   # some logic here af
 end
+
+string_hash = {
+  :bird => 3,
+  :word => 1,
+  :turd => 2,
+  :birds => 1
+}
+
+# Let's try using .each_with_object since it is more efficient because it doesn't create an intermediate array.
+
+string_hash.each do |key, value|
+  puts "Key: #{key}, Value: #{value}"
+end
