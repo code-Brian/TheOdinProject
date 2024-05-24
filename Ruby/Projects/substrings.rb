6277c3af2abb9ca573b3c63cd70a35918cd38e6f
Brian Denmark <89549557+code-Brian@users.noreply.github.com>
2024-05-23 17:52:18 -0700

    finished project

--- Ruby/Projects/substrings.rb
+++ Ruby/Projects/substrings.rb
@@ -1,12 +1,16 @@
 def substrings(string, dictionary)
-  # iterate through the dictionary and check if there are any matching keys
   words = string.downcase.split(" ")
   word_counts = Hash.new(0)
 
   words.each do |word|
     dictionary.each do |entry|
       if word.include?(entry)
-        puts "match with #{word} found at dictionary entry: #{entry}."
+        # puts "match with #{word} found at dictionary entry: #{entry}."
+        if word_counts.key?(entry)
+          word_counts[entry] += 1
+        elsif
+          word_counts[entry] = 1
+        end
       end
     end
   end
@@ -17,27 +21,4 @@ end
 dictionary = ["below", "down", "go", "going", "horn", "how", 
   "howdy", "it", "i", "low", "own", "part", "partner", "sit" ]
 
-substrings("below I go", dictionary)
-=begin
-
-to convert a string to a symbol, use .symbol_keys
-
-string_hash = {
-  :bird => 3,
-  :word => 1,
-  :turd => 2,
-  :birds => 1
-}
-
-# Let's try using .each_with_object since it is more efficient because it doesn't create an intermediate array.
-
-string_hash.each do |key, value|
-  puts "Key: #{key}, Value: #{value}"
-end
-
-puts dictionary
-=end
-
-
-
-
+substrings("Howdy partner, sit down! How's it going?", dictionary)
